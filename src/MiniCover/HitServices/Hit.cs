98a3b517c17f1d86b1f37b7a34a4ef30370d36c6
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-03-06 20:43:56 +0100

    Update tests

--- src/MiniCover/HitServices/Hit.cs
+++ src/MiniCover/HitServices/Hit.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Concurrent;
+﻿using Newtonsoft.Json;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace MiniCover
 {
