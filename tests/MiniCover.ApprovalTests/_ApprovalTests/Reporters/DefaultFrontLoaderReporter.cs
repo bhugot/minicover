f3c9ca53ac4369efda0b2d6d50af2e98dd5052d4
bhugot <bhugot@gmail.com>
2018-03-19 01:51:41 +0100

    Added source from ApprovalTests.Net and setuped for netcoreapp. Going to work on that later to remove and have a proper package.

---
+++ tests/MiniCover.ApprovalTests/_ApprovalTests/Reporters/DefaultFrontLoaderReporter.cs
@@ -0,0 +1,23 @@
+﻿namespace ApprovalTests.Reporters
+{
+    public class DefaultFrontLoaderReporter : FirstWorkingReporter
+    {
+        public static readonly DefaultFrontLoaderReporter INSTANCE = new DefaultFrontLoaderReporter();
+
+        public DefaultFrontLoaderReporter()
+            : base(
+            TfsReporter.INSTANCE,
+            TfsVnextReporter.INSTANCE,
+            TeamCityReporter.INSTANCE,
+            JenkinsReporter.INSTANCE,
+            BambooReporter.INSTANCE,
+            CruiseControlNetReporter.INSTANCE,
+            NCrunchReporter.INSTANCE,
+            MightyMooseAutoTestReporter.INSTANCE,
+            MyGetReporter.INSTANCE,
+            GoContinuousDeliveryReporter.INSTANCE,
+            AppVeyorReporter.INSTANCE)
+        {
+        }
+    }
+}
