f6026b865633a4e45e31773b89378e47ab79d678
Benjamin Hugot <bhugot@gmail.com>
2018-03-21 09:07:45 +0100

    fixed approval to work everywhere at least on windows for now

--- tests/MiniCover.ApprovalTests/ApprovaleHelper.cs
+++ tests/MiniCover.ApprovalTests/ApprovaleHelper.cs
@@ -1,8 +1,10 @@
 using System;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text;
 using ApprovalTests.Approvers;
 using ApprovalTests.Core;
+using ApprovalTests.Core.Exceptions;
 using ApprovalTests.Reporters;
 using ApprovalTests.Writers;
 
@@ -18,11 +20,22 @@ namespace MiniCover.ApprovalTests
 
         public static void VerifyXml(string text, [CallerFilePath] string filepath = null, [CallerMemberName] string membername = null)
         {
-            var writer =  WriterFactory.CreateTextWriter(text, ".xml");
+            var writer = WriterFactory.CreateTextWriter(text, ".xml");
             var filename = Path.GetFileNameWithoutExtension(filepath);
             var filedir = Path.Combine(Path.GetDirectoryName(filepath), "Result");
-            var namer = new SaneNamer {Name = filename + "." + membername, SourcePath = filedir};
-            var reporter = new MultiReporter(new TravisCIReporter());
+            var namer = new SaneNamer { Name = filename + "." + membername, SourcePath = filedir };
+            var reporter = new MultiReporter(new TravisCIReporter(), new DiffReporter());
+            Approver.Verify(new FileApprover(writer, namer), reporter);
+        }
+
+        public static void VerifyJson(string text, [CallerFilePath] string filepath = null, [CallerMemberName] string membername = null)
+        {
+            var writer = WriterFactory.CreateTextWriter(text, ".json");
+
+            var filename = Path.GetFileNameWithoutExtension(filepath);
+            var filedir = Path.Combine(Path.GetDirectoryName(filepath), "Result");
+            var namer = new SaneNamer { Name = filename + "." + membername, SourcePath = filedir };
+            var reporter = new MultiReporter(new TravisCIReporter(), new DiffReporter());
             Approver.Verify(new FileApprover(writer, namer), reporter);
         }
 
@@ -31,9 +44,25 @@ namespace MiniCover.ApprovalTests
             var writer = new ApprovalBinaryWriter(bytes);
             var filename = Path.GetFileNameWithoutExtension(filepath);
             var filedir = Path.Combine(Path.GetDirectoryName(filepath), "Result");
-            var namer = new SaneNamer {Name = filename + "." + membername, SourcePath = filedir};
-            var reporter = new MultiReporter(new TravisCIReporter());
-           Approver.Verify(new FileApprover(writer, namer), reporter);
+            var namer = new SaneNamer { Name = filename + "." + membername, SourcePath = filedir };
+            var reporter = new MultiReporter(new TravisCIReporter(), new DiffReporter());
+            Approver.Verify(new FileApprover(writer, namer), reporter);
+        }
+
+        internal static string ApplyCleanup(string text, params string[] pathToReplace)
+        {
+            var writer = new StringBuilder(text);
+            foreach (var toRemove in pathToReplace)
+            {
+                var pathNormalized = CleanPath(toRemove);
+                writer.Replace(pathNormalized, string.Empty);
+            }
+            return writer.ToString();
+        }
+
+        internal static string CleanPath(string path)
+        {
+           return path.Replace("/", "\\\\") + ("\\\\");
         }
     }
 }
--- tests/MiniCover.ApprovalTests/CoverageTests.cs
+++ tests/MiniCover.ApprovalTests/CoverageTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using Microsoft.DotNet.Cli.Utils;
 using MiniCover.HitServices;
 using Shouldly;
@@ -15,7 +16,8 @@ namespace MiniCover.ApprovalTests
             var sampleRootDirectory = Path.Combine(new SolutionDir().GetRootPath(this.GetType()), "sample");
             var sampleSolution = Path.Combine(sampleRootDirectory, "Sample.sln");
             var toolPath = Path.Combine(sampleRootDirectory, "tools");
-            var workdir = $"../coverage/{Guid.NewGuid()}";
+            var tempDirectory = Guid.NewGuid().ToString();
+            var workdir = $"../coverage/{tempDirectory}";
             var commandRestore = Command.CreateDotNet("restore", new[] {sampleSolution, "--no-cache"});
             var result = commandRestore.Execute();
             result.ExitCode.ShouldBe(0);
@@ -26,6 +28,10 @@ namespace MiniCover.ApprovalTests
             result = MiniCoverRunner.ExecuteInstrumenter(toolPath,workdir);
             result.ExitCode.ShouldBe(0);
             result.StdErr.ShouldBeNullOrEmpty();
+
+            var coverageJson = ApprovaleHelper.ApplyCleanup(File.ReadAllText(Path.Combine(toolPath, workdir, "coverage.json")), sampleRootDirectory, tempDirectory);
+            ApprovaleHelper.VerifyJson(coverageJson);
+
             result = MiniCoverRunner.ExecuteReset(toolPath, workdir);
             result.ExitCode.ShouldBe(0);
             result.StdErr.ShouldBeNullOrEmpty();
@@ -45,7 +51,73 @@ namespace MiniCover.ApprovalTests
             result.ExitCode.ShouldBe(0);
             result.StdErr.ShouldBeNullOrEmpty();
 
-            ApprovaleHelper.VerifyByte(File.ReadAllBytes(Path.Combine(toolPath,workdir, "coverage-hits.txt")));
+            var bytes = ReplaceTextInFile(Path.Combine(toolPath, workdir, "coverage-hits.txt"), sampleRootDirectory);
+         
+            ApprovaleHelper.VerifyByte(bytes);
         }
+
+        byte[] ReplaceTextInFile(string fileName, params string[] textToRemove)
+        {
+            byte[] fileBytes = File.ReadAllBytes(fileName);
+            
+            foreach(var toRemove in textToRemove)
+            fileBytes = ReplaceTextInByteArray(toRemove.Replace('/', '\\')+ '\\'
+                , string.Empty, fileBytes);
+
+            return fileBytes;
+        }
+
+        private byte[] ReplaceTextInByteArray(string oldText, string newText, byte[] fileBytes)
+        {
+            byte[] oldBytes = Encoding.UTF8.GetBytes(oldText);
+            byte[] newBytes = Encoding.UTF8.GetBytes(newText);
+            return ReplaceBytes(fileBytes, oldBytes, newBytes);
+        }
+
+        private byte[] ReplaceBytes(byte[] fileBytes, byte[] oldBytes, byte[] newBytes)
+        {
+            
+            byte[] newFileBytes = fileBytes;
+            while (true)
+            {
+                int index = IndexOfBytes(newFileBytes, oldBytes);
+                if (index < 0) break;
+                var tempileBytes =
+                new byte[newFileBytes.Length + newBytes.Length - oldBytes.Length];
+
+                Buffer.BlockCopy(newFileBytes, 0, tempileBytes, 0, index);
+                Buffer.BlockCopy(newBytes, 0, tempileBytes, index, newBytes.Length);
+                Buffer.BlockCopy(newFileBytes, index + oldBytes.Length,
+                    tempileBytes, index + newBytes.Length,
+                    newFileBytes.Length - index - oldBytes.Length);
+                newFileBytes = tempileBytes;
+            }
+            return newFileBytes;
+        }
+
+        int IndexOfBytes(byte[] searchBuffer, byte[] bytesToFind)
+        {
+            for (int i = 0; i < searchBuffer.Length - bytesToFind.Length; i++)
+            {
+                bool success = true;
+
+                for (int j = 0; j < bytesToFind.Length; j++)
+                {
+                    if (searchBuffer[i + j] != bytesToFind[j])
+                    {
+                        success = false;
+                        break;
+                    }
+                }
+
+                if (success)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
     }
 }
